46b6cbe4d8b48707d19ee2ad433b5814e755fe12
tjmsy <18753632+tjmsy@users.noreply.github.com>
2024-12-22 16:43:39 +0900

    feat-add-github-pages

---
+++ docs/site/main.js
@@ -0,0 +1,19 @@
+import { ScaleRatioControl } from 'https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-scale-ratio-dev@main/src/maplibre-gl-scale-ratio.js';
+
+const map = new maplibregl.Map({
+  container: 'map',
+  style: 'https://tiles.openfreemap.org/styles/liberty',
+  center: [0, 0],
+  zoom: 1,
+  hash: true,
+});
+map.addControl(
+  new MaplibreExportControl.MaplibreExportControl({
+    Crosshair: true,
+    PrintableArea: true,
+  }),
+  'top-right',
+);
+map.addControl(new ScaleRatioControl(), 'top-left');
+map.addControl(new maplibregl.NavigationControl(), 'bottom-right');
+map.addControl(new maplibregl.ScaleControl({ unit: 'metric' }), 'bottom-left');
